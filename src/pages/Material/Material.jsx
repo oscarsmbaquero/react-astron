356d0c07c0f81e08d7b1bb76787f0acaf885b2cb
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-10-02 18:01:11 +0200

    CircularProgressbar

--- src/pages/Material/Material.jsx
+++ src/pages/Material/Material.jsx
@@ -1,116 +1,179 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useReducer, useState } from "react";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
-import { BASE_URL } from '../../assets/ApiRoutes';
+import { BASE_URL } from "../../assets/ApiRoutes";
 import { useGetAuth } from "../../context";
 
-import  AddMaterial from './Components/AddMaterial'
-import  ListMaterial from './Components/ListMaterial'
+import "./Material.scss";
 
-import { tabsInitState, tabsReducer } from '../../utils/MaterialReducer'
+import AddMaterial from "./Components/AddMaterial";
+import ListMaterial from "./Components/ListMaterial";
+import ListMaterialById from "./Components/ListMaterialById";
 
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-import ListMaterialById from './Components/ListMaterialById';
+import { tabsInitState, tabsReducer } from "../../utils/MaterialReducer";
+import { Avatar } from "@mui/material";
+import { Button } from "@material-ui/core";
+
+import Fingerprint from '@mui/icons-material/Fingerprint';
+import { BsFillBrushFill } from "react-icons/bs";
+import { BsFillSignpost2Fill } from "react-icons/bs";
+
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import "react-circular-progressbar/dist/styles.css"
+
 
 const Material = () => {
-
   const loggedUser = useGetAuth();
-  const [material, setMaterial] = useState({})
-  const [users, setUsers] = useState([])
-  const [materialById, setMaterialById] = useState({})
+  const [material, setMaterial] = useState({});
+  const [users, setUsers] = useState([]);
+  const [materialById, setMaterialById] = useState({});
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  // const handleChange = (event, newValue) => {
+  //   setValue(newValue);
+  // };
   useEffect(() => {
     fetch(`${BASE_URL}/material`, {
-      //fetch("http://localhost:5000/material",{
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         // Authorization: `Bearer ${loggedUser.token}`
-      }
+      },
     })
-      .then(res => res.json())
-      .then(data => {
-        setMaterial(data)
-      })
+      .then((res) => res.json())
+      .then((data) => {
+        setMaterial(data);
+      });
   }, [loggedUser.token]);
-    
-   //console.log(loggedUser.id,44);
 
-   useEffect(() => {
+  //console.log(loggedUser.id,44);
+
+  useEffect(() => {
     fetch(`${BASE_URL}/users`)
-      .then(response => response.json())
-      .then(data => setUsers(data))
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
   }, []);
-  console.log(users,'users')
+  console.log(users, "users");
 
   const usersFiltered = users.filter(
-    (user) => user.account_type === 'Tecnico' || user.account_type === 'Admin'
+    (user) => user.account_type === "Tecnico" || user.account_type === "Admin"
   );
   //console.log(usersFiltered,'userFiltered')
-    
+
   useEffect(() => {
-      fetch(`${BASE_URL}/material/${loggedUser.id}`, {
-        //fetch("http://localhost:5000/material",{
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-           //Authorization: `Bearer ${loggedUser.token}`
-        }
-      })
-        .then(res => res.json())
-        .then(data => {
-          setMaterialById(data)
-        })
-    }, []);
-      console.log(materialById,'id');
+    fetch(`${BASE_URL}/material/${loggedUser.id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        //Authorization: `Bearer ${loggedUser.token}`
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setMaterialById(data);
+      });
+  }, [loggedUser.token]);
 
   const [tabs, dispatch] = useReducer(tabsReducer, tabsInitState);
 
   const AddTab = () => dispatch({ type: "ADD", payload: tabsInitState });
   const ListTab = () => dispatch({ type: "LIST" });
+  const TecnicoTab = () => dispatch({ type: "TECNICO" });
 
   const navigate = useNavigate();
 
- 
-  
-
- //console.log(usersFiltered,'userFiltered');
-
+  console.log(loggedUser, 76);
+  const percentage = 66;
+  //console.log(usersFiltered,'userFiltered');
 
   return (
-    <>
-    {!loggedUser ? <p>Cargando...</p> :(
-    <Box sx={{ width: '100%', typography: 'body1' }}>
-      <TabContext value={value}>
-        <Box sx={{ borderColor: 'divider' , display:'flex', justifyContent:'center', backgroundColor:'grey', textTransform:'lowercase'}}>
-          <TabList onChange={handleChange} aria-label="lab API tabs example">
-          
-            <Tab  label="Añadir Material" value="ADD" textColor="secondary" />
-            <Tab label="Mostrar Material" value="LIST" />
-            {loggedUser.rol ==='Admin' || loggedUser.rol ==='Tecnico' ?
-            <Tab label="Material por técnico" value="TECNICO" />:''}
-            
-          </TabList>
-        </Box>
-        <TabPanel value="ADD"><AddMaterial usersFiltered={usersFiltered}/></TabPanel>
-        <TabPanel value="LIST"><ListMaterial material={material}/></TabPanel>
-        <TabPanel value="TECNICO"><ListMaterialById materialById={materialById}/></TabPanel>
-        
-      </TabContext>
-    </Box>
-    ) 
-    }
-    </>
-    
-  )
-}
+    <section>
+      <div className="profile">
+        {/* <div className="profile__photo">
+          <Avatar
+            alt="Remy Sharp"
+            src={loggedUser.image}
+            className="profile__photo__img"
+          />
+        </div> */}
+        <div className="profile__personal">
+          <h1>{loggedUser.name}</h1>
+          <h2>{loggedUser.email}</h2>
+        </div>        
+        <div style={{ width: "12%" }}>
+        <p>Material Averiado</p>
+        <CircularProgressbar
+                    styles={buildStyles({
+                        pathColor: percentage > 100 ? '#DC2626' : '#3B82F6',
+                        trailColor: '#F5F5F5',
+                        textColor: percentage > 100 ? '#DC2626' : '#3B82F6',
+                    })}
+                    value={percentage}
+                    text={`${percentage} % `}
+                />
+        </div>
+      </div>
+      <div>
+        <div>
+          <ul className="ul">
+            <Button variant="contained" onClick={AddTab}  >
+              Añadir Material 
+              <BsFillBrushFill />
+            </Button>
+            <Button variant="contained"  onClick={ListTab}>
+              Total Msterial
+              <BsFillSignpost2Fill/>
+            </Button>
+            <Button variant="contained"  onClick={TecnicoTab}>
+               Material Técnico
+               <Fingerprint />
+            </Button>
+          </ul>
+          {tabs.addMaterial && (
+            <>
+              <AddMaterial usersFiltered={usersFiltered} />
+            </>
+          )}
+          {tabs.listMaterial && (
+            <>
+              <ListMaterial material={material} />
+            </>
+          )}
+          {tabs.tecnicoMaterial && (
+            <>
+              <ListMaterialById materialById={materialById} />
+            </>
+          )}
+        </div>
+      </div>
+    </section>
 
-export default Material
+    //esta section funciona, pestañas de material-ui //pueden valer 02-10-2022- Oscar
+
+    // <>
+    // {!loggedUser ? <p>Cargando...</p> :(
+    // <Box sx={{ width: '100%', typography: 'body1' }}>
+    //   <TabContext value={value}>
+    //     <Box sx={{ borderColor: 'divider' , display:'flex', justifyContent:'center', backgroundColor:'grey', textTransform:'lowercase'}}>
+    //       <TabList onChange={handleChange} aria-label="lab API tabs example">
+
+    //         <Tab  label="Añadir Material" value="ADD" textColor="secondary" />
+    //         <Tab label="Mostrar Material" value="LIST" />
+    //         {loggedUser.rol ==='Admin' || loggedUser.rol ==='Tecnico' ?
+    //         <Tab label="Material por técnico" value="TECNICO" />:''}
+
+    //       </TabList>
+    //     </Box>
+    //     <TabPanel value="ADD"><AddMaterial usersFiltered={usersFiltered}/></TabPanel>
+    //     <TabPanel value="LIST"><ListMaterial material={material}/></TabPanel>
+    //     <TabPanel value="TECNICO"><ListMaterialById materialById={materialById}/></TabPanel>
+
+    //   </TabContext>
+    // </Box>
+    // )
+    // }
+    // </>
+  );
+};
+
+export default Material;
