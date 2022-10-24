4d261b5a29bdf295d1bd52cc0aa9bf297e62ba0d
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-10-24 20:42:15 +0200

    change at form AddAviso

--- src/pages/Avisos/AñadirAvisos/AddAvisos.jsx
+++ src/pages/Avisos/AñadirAvisos/AddAvisos.jsx
@@ -1,91 +1,155 @@
-import React from 'react';
-import { useForm } from 'react-hook-form'
-import {  useNavigate } from "react-router-dom";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../../../assets/ApiRoutes";
-import './AddAvisos.scss';
-import Loader from '../../../core/components/Loader/Loader';
-import Swal from 'sweetalert2'// hay que probarlo
-import { Button, Container, FormControl, FormHelperText, Grid, Input, InputLabel,  } from '@mui/material';
-import SendIcon from '@mui/icons-material/Send';
+import "./AddAvisos.scss";
+import Loader from "../../../core/components/Loader/Loader";
+import Swal from "sweetalert2"; // hay que probarlo
+import {
+  Button,
+  Container,
+  FormControl,
+  FormHelperText,
+  Grid,
+  Input,
+  InputLabel,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import logo from "../../../assets/images/logo.jpg";
 
 //import { Container, FormControl, InputLabel, Input, FormHelperText} from '@mui/material/FormControl';
 
-
-
 const AddAvisos = () => {
-
-    const {register, handleSubmit, formState: {errors, isValid}} = useForm({mode: "onChange"});
-    let navigate = useNavigate();
-    const onSubmit = async (formData) => {
-        console.log(formData,15);
-        try {
-
-            const result = await fetch(`${BASE_URL}/avisos` ,{
-                method: "POST",
-                headers: {
-                        'Content-Type': 'application/json'
-               },
-               body: JSON.stringify(formData),
-            })
-            const resData = await result.json();
-            Swal.fire({
-                title: 'Success!',
-                text: 'Aviso introducido Correctamente',
-                icon: 'success',
-                confirmButtonText: 'Ok'
-              })
-            navigate("/avisos/caceres");
-            console.log(resData);
-            
-            
-            
-        } catch (error) {
-           console.log(error); 
-        }
-        
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({ mode: "onChange" });
+  let navigate = useNavigate();
+  const onSubmit = async (formData) => {
+    console.log(formData, 15);
+    try {
+      const result = await fetch(`${BASE_URL}/avisos`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const resData = await result.json();
+      Swal.fire({
+        title: "Success!",
+        text: "Aviso introducido Correctamente",
+        icon: "success",
+        confirmButtonText: "Ok",
+      });
+      navigate("/avisos/caceres");
+      console.log(resData);
+    } catch (error) {
+      console.log(error);
     }
+  };
   return (
-    <section className="sectionEdit">
-        <div className="edit">
-        <h3>Nuevo Aviso</h3>
-            <form onSubmit={handleSubmit(onSubmit)} className="edit__form">
-            
-                <label className="edit__label">Incidencia</label>
-                    <input className="edit__input" type="text" name="n_incidencia" placeholder="Nº Incidencia" {...register('n_incidencia')}/>
-                <label className="edit__label">Centro</label>
-                    <input className="edit__input" type="text" name="centro" placeholder="Centro"  {...register('centro')}/>
-                <label className="edit__label">Localidad</label>
-                    <input className="edit__input" type="text" name="localidad" placeholder="Localidad"  {...register('localidad')}/>
-                <label className="edit__label">Provincia</label>
-                    <select  {...register("provincia")} className='edit__select'>
-                        <option value="Cáceres">Cáceres</option>
-                        <option value="Badajoz">Badajoz</option>
-                    </select>
+    <>
+      <div className="container">
+        <section className="sectionForm row">
+          <div className="col-12 col-lg-12 my-auto">
+          <h3>Añadir Aviso</h3>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              {/* <img className="logoReservas1" src={logo} alt="logo"></img> */}
+              <br />
+              <div className="d-flex flex-column flex-md-row">
+                <div className="d-flex flex-column col-11 col-md-4 ">
+                  <label className="form__label">Incidencia * </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="n_incidencia"
+                    placeholder="Nº Incidencia"
+                    {...register("n_incidencia")}
+                  />
+                </div>
+                <div className="d-flex flex-column col-11 col-md-8 mx-md-3 ">
+                  <label className="form__label">Centro * </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="centro"
+                    placeholder="Centro"
+                    {...register("centro")}
+                  />
+                </div>
+              </div>
+              <div className="d-flex flex-column flex-md-row">
+                <div className="d-flex flex-column col-11 col-md-6 ">
+                  <label className="form__label">Localidad * </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="localidad"
+                    placeholder="Localidad"
+                    {...register("localidad")}
+                  />
+                </div>
+                <div className="d-flex flex-column col-11 col-md-6 mx-md-3  ">
+                  <label className="form__label">Provincia * </label>
+                  <select {...register("provincia")} className="form-control">
+                    <option value="Cáceres">Cáceres</option>
+                    <option value="Badajoz">Badajoz</option>
+                  </select>
+                </div>
+              </div>
+              <div className="d-flex flex-column flex-md-row">
+                <div className="d-flex flex-column col-11 col-lg-12 col-md-6 mx-md-1 ">
+                  <label className="form__label">Averia * </label>
+                  <textarea
+                    className="form-control"
+                    type="text"
+                    name="averia"
+                    placeholder="Descripción averia"
+                    {...register("averia")}
+                  />
+                </div>
+              </div>
+              <div className="d-flex flex-column flex-md-row">
+                <div className="d-flex flex-column col-11 col-md-6 ">
+                  <label className="form__label">Prioridad * </label>
+                  <select {...register("prioridad")} className="form-control">
+                    <option value="Urgente">Urgente</option>
+                    <option value="Normal">Normal</option>
+                  </select>
+                </div>
+                <div className="d-flex flex-column col-11 col-md-6 mx-md-3 ">
+                  <label className="form__label">Estado * </label>
+                  <select {...register("estado")} className="form-control">
+                    <option value="Abierta" selected>Abierta</option>
+                    
+                  </select>
+                  
+                </div>
+              </div>
 
-                {/* <label className="edit__label">Provincia</label>
-                    <input className="edit__input" type="text" name="provincia" placeholder="Provincia"  {...register('provincia')}/> */}
-                <label className="edit__label">Averia</label>
-                    <textarea className='edit__text-area' type="text" name="averia" placeholder="Descripciòn averia"  {...register('averia')}/>
-                <label className="edit__label">Prioridad</label>
-                    <select  {...register("prioridad")} className='edit__select'>
-                        <option value="Urgente">Urgente</option>
-                        <option value="Normal">Normal</option>
-                    </select>
-                <label className="edit__label">Estado</label>
-                    <select  {...register("estado")}  className='edit__select'>
-                        <option value="Abierta" selected>Abierto</option>
-                        {/* <option value="Asignado">Asignado</option>
-                        <option value="Cerrado">Cerrado</option> */}
-                    </select>
-                <br></br>
-                <Button variant="contained" type='submit' endIcon={<SendIcon />} >
-                    Enviar
-                </Button>
+              <br />
+              <Button
+                variant="contained"
+                //color="primary"
+                type="submit"
+                endIcon={<SendIcon />}
+                style={{
+                  borderRadius: 50,
+                  backgroundColor: "black",
+                  color: "white",
+                  // marginTop:'0px'
+                }}
+              >
+                Enviar
+              </Button>
             </form>
-        </div>
-    </section>
-  )
-  
-}
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
 
-export default AddAvisos
+export default AddAvisos;
