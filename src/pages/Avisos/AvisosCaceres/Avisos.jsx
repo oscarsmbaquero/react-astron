00f4f474af968926e87a437896fc259aef029e8f
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-11-04 19:48:25 +0100

    pagination and title at table avisos

--- src/pages/Avisos/AvisosCaceres/Avisos.jsx
+++ src/pages/Avisos/AvisosCaceres/Avisos.jsx
@@ -1,67 +1,69 @@
-import React,{ useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { BASE_URL } from "../../../assets/ApiRoutes";
-import DataTable from 'react-data-table-component';
-import Badge from 'react-bootstrap/Badge';
+import DataTable from "react-data-table-component";
+import Badge from "react-bootstrap/Badge";
 
 const Avisos = () => {
+  let [avisos, SetAvisos] = useState([]);
 
-    let [avisos, SetAvisos] = useState([]);
+  useEffect(() => {
+    fetch(`${BASE_URL}/avisos`)
+      .then((response) => response.json())
+      .then((data) => SetAvisos(data));
+  }, []);
 
-    useEffect(() => {
-        fetch(`${BASE_URL}/avisos`)
-          .then((response) => response.json())
-          .then((data) => SetAvisos(data));
-      }, []);
+  console.log(avisos, "avisos");
 
-console.log(avisos,'avisos');
-
-const columns = [
+  const columns = [
     {
-        name: 'Incidencia',
-        selector: row=> row.n_incidencia,
+      name: "Incidencia",
+      selector: (row) => row.n_incidencia,
     },
     {
-        name: 'Centro',
-        selector: row => row.centro,
+      name: "Centro",
+      selector: (row) => row.centro,
     },
     {
-        name: 'Estado',
-        selector: row=> row.estado === 'Pendiente' ?
-         <Badge bg="danger">{row.estado}</Badge>:
-         row.estado === 'Abierta' ?
-         <Badge bg="primary">{row.estado}</Badge>:
-        <Badge bg="success">{row.estado}</Badge>
+      name: "Estado",
+      selector: (row) =>
+        row.estado === "Pendiente" ? (
+          <Badge bg="danger">{row.estado}</Badge>
+        ) : row.estado === "Abierta" ? (
+          <Badge bg="primary">{row.estado}</Badge>
+        ) : (
+          <Badge bg="success">{row.estado}</Badge>
+        ),
     },
     {
-        name: 'Localidad',
-        selector: row => row.localidad,
+      name: "Localidad",
+      selector: (row) => row.localidad,
     },
-];
-// const data = [
-//     {
-//         id: 1,
-//         title: 'Beetlejuice',
-//         year: '1988',
-//     },
-//     {
-//         id: 2,
-//         title: 'Ghostbusters',
-//         year: '1984',
-//     },
-//     {
-//         id: 3,
-//         title: 'Ghostbusters',
-//         year: '1984',
-//     },
-// ]
-return (
-    <DataTable
-        columns={columns}
-        data={avisos}
-    />
-);
+  ];
+  // const data = [
+  //     {
+  //         id: 1,
+  //         title: 'Beetlejuice',
+  //         year: '1988',
+  //     },
+  //     {
+  //         id: 2,
+  //         title: 'Ghostbusters',
+  //         year: '1984',
+  //     },
+  //     {
+  //         id: 3,
+  //         title: 'Ghostbusters',
+  //         year: '1984',
+  //     },
+  // ]
+  return (
+      <DataTable 
+      title="Avisos Totales"
+      columns={columns} 
+      data={avisos} 
+      pagination
+      />
+  );
 };
-  
 
-
-export default Avisos
+export default Avisos;
