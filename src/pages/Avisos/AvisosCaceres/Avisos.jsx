9d293aadab4a0995c6ff8acefa086b1fd6e7ec68
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-11-05 00:34:05 +0100

    datatable with bottons detail ok

--- src/pages/Avisos/AvisosCaceres/Avisos.jsx
+++ src/pages/Avisos/AvisosCaceres/Avisos.jsx
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from "react";
 import { BASE_URL } from "../../../assets/ApiRoutes";
 import DataTable from "react-data-table-component";
 import Badge from "react-bootstrap/Badge";
+import { AddIcCallOutlined, Create, DeleteOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { Link } from "react-router-dom";
 
 const Avisos = () => {
   let [avisos, SetAvisos] = useState([]);
+  
 
   useEffect(() => {
     fetch(`${BASE_URL}/avisos`)
@@ -14,14 +19,19 @@ const Avisos = () => {
 
   console.log(avisos, "avisos");
 
+  const clickHandler = (e, id) => {
+    console.log(id);
+  };
   const columns = [
     {
       name: "Incidencia",
       selector: (row) => row.n_incidencia,
+      sortable: true,
     },
     {
       name: "Centro",
       selector: (row) => row.centro,
+      sortable: true,
     },
     {
       name: "Estado",
@@ -33,12 +43,37 @@ const Avisos = () => {
         ) : (
           <Badge bg="success">{row.estado}</Badge>
         ),
+      sortable: true,
     },
     {
       name: "Localidad",
       selector: (row) => row.localidad,
+      sortable: true,
+    },
+    {
+      name: "Acciones",
+      // selector: (row) => row.localidad,
+      cell: (row) => (
+        //
+        <>
+        {console.log(row._id)}
+          <Link to={`/avisos/details/${row._id}`}>
+            <IconButton aria-label="delete" color="success">
+              <AddIcon />
+            </IconButton>
+          </Link>
+          <IconButton color="error" onClick={(e) => clickHandler(e, row._id)}>
+            <DeleteOutlined />
+          </IconButton>
+          ,
+        </>
+      ),
+      ignoreRowClick: true,
+      allowOverflow: true,
+      button: true,
     },
   ];
+
   // const data = [
   //     {
   //         id: 1,
@@ -57,12 +92,13 @@ const Avisos = () => {
   //     },
   // ]
   return (
-      <DataTable 
+    <DataTable
       title="Avisos Totales"
-      columns={columns} 
-      data={avisos} 
+      columns={columns}
+      data={avisos}
+      // expandableRows expandableRowsComponent={ExpandedComponent}
       pagination
-      />
+    />
   );
 };
 
--- src/pages/Avisos/AvisosCaceres/AvisosDetail.jsx
+++ src/pages/Avisos/AvisosCaceres/AvisosDetail.jsx
@@ -1,9 +1,16 @@
+import { CardActions, CardContent, CardHeader, Grid, IconButton, Typography } from '@mui/material';
 import React,{ useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom';
+import { Badge, Card, Container } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
 import { BASE_URL } from "../../../assets/ApiRoutes";
 import './AvisosCaceres.scss';
+import { useGetAuth } from "../../../context/context";
+import { Create, DeleteOutlined } from '@mui/icons-material';
+import AddIcon from "@mui/icons-material/Add";
+import Button from "@mui/material/Button";
 
 const AvisosDetail = () => {
+  const userLogged = useGetAuth();
   const { id } = useParams();
   console.log(id,'param');
 
@@ -19,8 +26,9 @@ const AvisosDetail = () => {
  console.log(avisos,'llego')
  return (
   <div>
-  { !avisos ? <p>Cargando...</p> : <>
-    <div className="details">
+  { !avisos ? <p>Cargando...</p> : 
+  <>
+    {/* <div className="details">
       <div className="details__text">      
           <h1>{avisos.n_incidencia}</h1>
           <h2>{avisos.localidad}</h2>          
@@ -29,7 +37,146 @@ const AvisosDetail = () => {
           
           <p>{avisos.averia}</p>          
        </div>
-    </div>
+    </div> */}
+    <Container>
+        <Grid container spacing={5}>
+          
+            <Grid item key={avisos._id} xs={12} md={12} lg={12}>
+              <Card
+                elevation={5}
+                sx={{
+                  borderRadius: "4px",
+                  justifyContent: "flex-start",
+                  flex: "1 0 auto",
+                  flexDirection: "column",
+                  flexWrap: "wrap",
+                  ":hover": {
+                    boxShadow: 20, // theme.shadows[20]
+                  },
+                }}
+              >
+                <CardHeader
+                  sx={{
+                    //color: "white.main",
+                    background: "grey",
+                    justifyContent: "space-between",
+                  }}
+                  title ={avisos.centro}
+                  titleStyle={{textAlign: 'center'}}
+                  action={
+                    <>
+                      {" "}
+                      {userLogged.rol === "Tecnico" ? (
+                        ""
+                      ) : (
+                        <IconButton
+                          color="error"
+                          // onClick={(e) => deleteaviso(e, avisos._id)}
+                        >
+                          <DeleteOutlined />
+                        </IconButton>
+                      )}
+                      <Link to={`/edit/avisos/${avisos._id}`}>
+                        <IconButton aria-label="delete" color="secondary">
+                          <Create />
+                        </IconButton>
+                      </Link>
+                      <Link to={`/avisos/details/${avisos._id}`}>
+                        <IconButton aria-label="delete" color="success">
+                          <AddIcon />
+                        </IconButton>
+                      </Link>
+                    </>
+                  }
+                  // title ={avisos.n_incidencia}
+                   
+                />
+                <CardContent sx={{ flex: "2 0 auto" }}>
+                  <Typography
+                    variant="body1"
+                    color="error"
+                    sx={{ fontSize: 26 }}
+                  >
+                  <Badge bg="danger">{avisos.n_incidencia}</Badge><br/>
+                  {avisos.prioridad === 'Urgente' ?
+                  <Badge bg="warning">{avisos.prioridad}</Badge>
+                  :
+                  <Badge bg="success">{avisos.prioridad}</Badge>
+                  }
+                  
+                  </Typography>
+                  <Typography variant="h4" component={"div"}>
+                    {avisos.localidad}
+                  </Typography>
+                  <Typography sx={{ fontSize: 26 }} color="text.secondary">
+                    {avisos.centro}
+                  </Typography>
+                  <Typography variant="h6">
+                    {
+                      avisos.estado === "Abierta" ? ( //primera condicion
+                        <Badge bg="success"> {avisos.estado}</Badge>
+                      ) : avisos.estado === "Pendiente" ? ( //segunda condicion
+                        <Badge bg="warning"> {avisos.estado}</Badge>
+                      ) : (
+                        <Badge bg="primary">
+
+                          {" "}
+                          {avisos.estado}:&nbsp;{avisos.user_assigned?.name}
+                        </Badge>
+                      ) //si no cumple tercera condicion
+                    }
+                  </Typography>
+                  <Typography variant="h6"></Typography>
+                  <Typography variant="body1" color="text.secondary">
+                    {avisos.averia}
+                  </Typography>
+                </CardContent>
+                <CardActions
+                  sx={{
+                    justifyContent: "center",
+                  }}
+                >
+                  {avisos.estado === "Asignado" ? (
+                    <Button variant="contained" color="error" size="small">
+                      <Link
+                        to={`/avisos/reasignar/${avisos._id}/${avisos.user_assigned?._id}`}
+                      >
+                        Reasignar
+                      </Link>
+                    </Button>
+                  ) : (
+                    <Button variant="contained" size="small">
+                      <Link
+                        to={`/avisos/asignar/${avisos._id}/${avisos.n_incidencia}/${avisos.centro}`}
+                      >
+                        Asignar
+                      </Link>
+                    </Button>
+                  )}
+                  {avisos.user_assigned?.name === userLogged.name &&
+                  <Button variant="contained" color="warning" size="small">
+                    <Link to={`/avisos/intervencion/${avisos._id}`}>
+                      Añadir Int.
+                    </Link>
+                  </Button>
+                  }
+                  { avisos.intervencion.length !== 0 &&
+                  <Button variant="contained" color="success" size="small">
+                    <Link to={`/mostrar/intervencion/${avisos._id}`}>
+                      Mostrar Int.
+                    </Link>
+                  </Button>
+                  }
+                </CardActions>
+              </Card>
+            </Grid>
+          ))
+          
+          
+          
+        </Grid>
+       
+      </Container>
   </>}
 </div>
    );
