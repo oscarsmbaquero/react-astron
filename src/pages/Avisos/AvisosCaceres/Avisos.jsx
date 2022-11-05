ace214f8148deed5d6d795fe7c9dcf634d2be7c2
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-11-05 09:56:29 +0100

    tabs material Caceres

--- src/pages/Avisos/AvisosCaceres/Avisos.jsx
+++ src/pages/Avisos/AvisosCaceres/Avisos.jsx
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { BASE_URL } from "../../../assets/ApiRoutes";
-import DataTable from "react-data-table-component";
-import Badge from "react-bootstrap/Badge";
-import { AddIcCallOutlined, Create, DeleteOutlined } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
-  import AddIcon from "@mui/icons-material/Add";
-import { Link } from "react-router-dom";
+// import DataTable from "react-data-table-component";
+// import Badge from "react-bootstrap/Badge";
+// import { AddIcCallOutlined, Create, DeleteOutlined } from "@mui/icons-material";
+// import { IconButton } from "@mui/material";
+//   import AddIcon from "@mui/icons-material/Add";
+
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+
+//import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import TotalCaceres from "./components/TotalCaceres";
+import { Typography } from "@mui/material";
+
 
 const Avisos = () => {
   let [avisos, SetAvisos] = useState([]);
@@ -16,91 +26,67 @@ const Avisos = () => {
       .then((response) => response.json())
       .then((data) => SetAvisos(data));
   }, []);
+  const [value, setValue] = React.useState(0);
 
-  console.log(avisos, "avisos");
-
-  const clickHandler = (e, id) => {
-    console.log(id);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
-  const columns = [
-    {
-      name: "Incidencia",
-      selector: (row) => <Badge bg="primary" text="bold">{row.n_incidencia}</Badge>,
-      sortable: true,
-    },
-    {
-      name: "Centro",
-      selector: (row) => row.centro,
-      sortable: true,
-      
-    },
-    {
-      name: "Estado",
-      selector: (row) =>
-        row.estado === "Pendiente" ? (
-          <Badge bg="danger">{row.estado}</Badge>
-        ) : row.estado === "Abierta" ? (
-          <Badge bg="primary">{row.estado}</Badge>
-        ) : (
-          <Badge bg="success">{row.estado}</Badge>
-        ),
-      sortable: true,
-    },
-    {
-      name: "Localidad",
-      selector: (row) => row.localidad,
-      sortable: true,
-    },
-    {
-      name: "Acciones",
-      // selector: (row) => row.localidad,
-      cell: (row) => (
-        //
-        <>
-        {console.log(row._id)}
-          <Link to={`/avisos/details/${row._id}`}>
-            <IconButton aria-label="delete" color="success">
-              <AddIcon />
-            </IconButton>
-          </Link>
-          <IconButton color="error" onClick={(e) => clickHandler(e, row._id)}>
-            <DeleteOutlined />
-          </IconButton>
-          ,
-        </>
-      ),
-      ignoreRowClick: true,
-      allowOverflow: true,
-      button: true,
-    },
-  ];
 
-  // const data = [
-  //     {
-  //         id: 1,
-  //         title: 'Beetlejuice',
-  //         year: '1988',
-  //     },
-  //     {
-  //         id: 2,
-  //         title: 'Ghostbusters',
-  //         year: '1984',
-  //     },
-  //     {
-  //         id: 3,
-  //         title: 'Ghostbusters',
-  //         year: '1984',
-  //     },
-  // ]
+  
   return (
-    <DataTable
-      // title="Avisos Totales"
-      columns={columns}
-      data={avisos}
-      // expandableRows expandableRowsComponent={ExpandedComponent}
-      pagination
-      dense
-    />
+    <>
+    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="Total Avisos Cáceres" />
+        <Tab label="Abiertos" />
+        <Tab label="Pendientes" />
+        <Tab label="Cerrados" />
+      </Tabs>
+    </Box>
+    <Box sx={{ padding: 2 }}>
+        {value === 0 && (
+          <Box>
+          <TotalCaceres avisos={avisos}/>   
+          </Box>
+        )}
+        {value === 1 && (
+          <Box>
+            <Typography>The second tab</Typography>
+          </Box>
+        )}
+        {value === 2 && (
+          <Box>
+            <Typography>The third tab</Typography>
+          </Box>
+        )}
+        {value === 3 && (
+          <Box>
+            <Typography>The four tab</Typography>
+          </Box>
+        )}
+      </Box>
+      {/* <Tabs id="controlled-tabs" selectedTabClassName="bg-danger" >
+    <TabList>
+      <Tab
+      
+      >Total</Tab>
+      <Tab>Abiertos</Tab>
+      <Tab>Pendientes</Tab>
+      <Tab>Cerrados</Tab>
+    </TabList>
+
+    <TabPanel>
+    <TotalCaceres avisos={avisos}/>   
+    </TabPanel>
+    <TabPanel>   
+    </TabPanel>
+    <TabPanel>   
+    </TabPanel>
+    <TabPanel>
+    </TabPanel>
+  </Tabs> */}
+    
+    </>
   );
 };
 
---
+++ src/pages/Avisos/AvisosCaceres/components/TotalCaceres.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import DataTable from "react-data-table-component";
+import Badge from "react-bootstrap/Badge";
+import { AddIcCallOutlined, Create, DeleteOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+  import AddIcon from "@mui/icons-material/Add";
+  import { Link } from "react-router-dom";
+
+const TotalCaceres = ({avisos}) => {
+
+    const clickHandler = (e, id) => {
+        console.log(id);
+      };
+      const columns = [
+        {
+          name: "Incidencia",
+          selector: (row) => <Badge bg="primary" text="bold">{row.n_incidencia}</Badge>,
+          sortable: true,
+        },
+        {
+          name: "Centro",
+          selector: (row) => row.centro,
+          sortable: true,
+          
+        },
+        {
+          name: "Estado",
+          selector: (row) =>
+            row.estado === "Pendiente" ? (
+              <Badge bg="danger">{row.estado}</Badge>
+            ) : row.estado === "Abierta" ? (
+              <Badge bg="primary">{row.estado}</Badge>
+            ) : (
+              <Badge bg="success">{row.estado}</Badge>
+            ),
+          sortable: true,
+        },
+        {
+          name: "Localidad",
+          selector: (row) => row.localidad,
+          sortable: true,
+        },
+        {
+          name: "Acciones",
+          // selector: (row) => row.localidad,
+          cell: (row) => (
+            //
+            <>
+            {console.log(row._id)}
+              <Link to={`/avisos/details/${row._id}`}>
+                <IconButton aria-label="delete" color="success">
+                  <AddIcon />
+                </IconButton>
+              </Link>
+              <IconButton color="error" onClick={(e) => clickHandler(e, row._id)}>
+                <DeleteOutlined />
+              </IconButton>
+              ,
+            </>
+          ),
+          ignoreRowClick: true,
+          allowOverflow: true,
+          button: true,
+        },
+      ];
+  
+
+
+    console.log(avisos,'avisos abiertos');
+  return (
+      <DataTable
+    //   title="Avisos Totales"
+      columns={columns}
+      data={avisos}
+      pagination
+      dense
+    /> 
+  )
+}
+
+export default TotalCaceres
