4c9dd1f4ca3ce7b4c0bbd7c7dfd048de70f0493b
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-11-08 20:13:56 +0100

    home changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,9 +6,10 @@ import MuiAccordion from "@mui/material/Accordion";
 import MuiAccordionSummary from "@mui/material/AccordionSummary";
 import MuiAccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
-
+import { useGetAuth } from "../../context/context";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import fondo from '../../../src/assets/images/fondo.jpg'
 
 const resultadoOperativo = 80;
 const resultadoAveriado = 20;
@@ -50,6 +51,7 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
 }));
 
 export default function Home() {
+  const userLogged = useGetAuth();
   const [expanded, setExpanded] = React.useState("panel1");
 
   const handleChange = (panel) => (event, newExpanded) => {
@@ -57,13 +59,17 @@ export default function Home() {
   };
 
   return (
+
+  
     <div>
+      {userLogged.id ?
+      <>
       <Accordion
-        expanded={expanded === "panel1"}
-        onChange={handleChange("panel1")}
+        expanded={expanded === "panel2"}
+        onChange={handleChange("panel2")}
       >
-        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography sx={{ color: "text.secondary" }}>
+        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+          <Typography sx={{ color: "text.secondary", alignContent: "center" }}>
             Graficos Cáceres
           </Typography>
         </AccordionSummary>
@@ -90,17 +96,17 @@ export default function Home() {
               <CircularProgressbar
                 styles={buildStyles({
                   pathColor:
-                    resultadoOperativo > 60
+                  resultadoAveriado > 60
                       ? "#DC2626"
-                      : resultadoOperativo < 60 && resultadoOperativo > 40
+                      : resultadoAveriado < 60 && resultadoAveriado > 40
                       ? "#d8f007"
                       : "#35DE0B",
                   trailColor: "#F5F5F5",
                   //textColor: resultadoOperativo > 60 ? '#DC2626' :resultadoOperativo < 60 && resultadoOperativo > 40 ? '#d8f007': '#35DE0B',
                   textColor: "black",
                 })}
-                value={resultadoOperativo}
-                text={`${resultadoOperativo} % `}
+                value={resultadoAveriado}
+                text={`${resultadoAveriado} % `}
               />
             </div>
             <div className="graficas">
@@ -188,6 +194,13 @@ export default function Home() {
           </div>
         </AccordionDetails>
       </Accordion>
+      </>
+      :
+      <div className="home">
+        <img src={fondo}alt="Fondo" className="image"></img>
+      </div>
+      }
+    
     </div>
   );
 }
